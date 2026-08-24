87e3143c0f20966abd366c8fa084d4ad2237ffd0
zmzhang <8111354+zmzhang8@users.noreply.github.com>
2026-08-23 19:24:13 -0700

    Update package script

--- package.sh
+++ package.sh
@@ -2,4 +2,4 @@
 
 glib-compile-schemas --strict schemas
 rm -f extension.zip
-zip -r extension.zip schemas/ *.js metadata.json LICENSE README.md
+zip -r extension.zip schemas/*.xml *.js metadata.json LICENSE README.md
